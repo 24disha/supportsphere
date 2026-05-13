1cc63a6ca5b2be50d57256afcb4fa21978c7724a
24disha <156945810+24disha@users.noreply.github.com>
2026-05-13 23:18:31 +0530

    fixed app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { useState, useEffect } from "react";
-
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
-import Loader from "./components/Loader";
 
 function App() {
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-
-    setTimeout(() => {
-
-      setLoading(false);
-
-    }, 2000);
-
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
   return (
-
     <BrowserRouter>
-
       <Routes>
-
         <Route path="/" element={<Login />} />
-
         <Route
           path="/dashboard"
           element={<Dashboard />}
         />
-
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
 
 export default App;
